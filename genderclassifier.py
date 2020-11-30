3fddbf7f2863939aa052959ed5a45c14780ef061
arnav-verma22 <arn.verma22@gmail.com>
2020-11-30 21:45:27 +0530

    tf model created

--- genderclassifier.py
+++ genderclassifier.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 df = pd.read_csv("gender-classifier-DFE-791531.csv", encoding="ISO-8859-1")
 
@@ -24,7 +25,7 @@ mv = SimpleImputer(missing_values=np.nan, strategy="constant", fill_value= 'othe
 mv = mv.fit([x['gender']])
 [x['gender']] = mv.transform([x['gender']])
 
-colums_to_encoded = ['_unit_state', 'gender', 'link_color', 'sidebar_color']
+colums_to_encoded = ['_unit_state', 'link_color', 'sidebar_color']
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
@@ -35,9 +36,27 @@ colums_to_scale = ['_trusted_judgments', 'link_color', 'sidebar_color',
                    'fav_number', 'retweet_count', 'tweet_count']
 
 y = x['gender']
-x = x.drop(columns=x['gender'])
+x = x.drop(columns=['gender', 'description', 'text', 'name'])
+
+
 
 from sklearn.preprocessing import StandardScaler
-for j in colums_to_scale:
-    sc_X = StandardScaler()
-    [x[j]] = sc_X.fit_transform([x[j]])
+sc_X = StandardScaler()
+x = sc_X.fit_transform(x)
+
+y = pd.get_dummies(y)
+
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.25, random_state=0)
+
+ann = tf.keras.models.Sequential()
+ann.add(tf.keras.layers.Dense(units=8, activation='tanh'))
+ann.add(tf.keras.layers.Dense(units=7, activation='tanh'))
+ann.add(tf.keras.layers.Dense(units=5, activation='softmax'))
+ann.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+ann.fit(xtrain, ytrain, batch_size = 32, epochs = 100)
+
+nn_prediction = ann.predict(xtest)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+print(accuracy_score(ytest, nn_prediction))
