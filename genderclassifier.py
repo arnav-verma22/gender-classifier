f09dcfc058c52bec0ff44719529ca7a05742710c
arnav-verma22 <arn.verma22@gmail.com>
2020-11-30 07:21:09 +0530

    columns to remove

--- genderclassifier.py
+++ genderclassifier.py
@@ -1,3 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv("gender-classifier-DFE-791531.csv", encoding="ISO-8859-1")
+
+for i in df.columns:
+    print(i, len(df[i].unique()))
+
+columns = ['_golden', '_last_judgment_at', 'profile_yn', 'profile_yn:confidence', 'created', 'gender_gold',
+           'profile_yn_gold', 'profileimage', 'tweet_coord', 'tweet_created', 'tweet_location', 'user_timezone']
